0cbb70b2f58e8842b708c2ace5c4040b6ca1d2f5
Steph Wong <12522345+stephwongwt@users.noreply.github.com>
2018-07-19 14:07:50 +0800

    Update MoveThor.py
    Shortened code

--- CodinGame/MoveThor.py
+++ CodinGame/MoveThor.py
@@ -1,40 +1,20 @@
-import sys
-import math
-
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-# ---
-# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
-
-# light_x: the X position of the light of power
-# light_y: the Y position of the light of power
-# initial_tx: Thor's starting X position
-# initial_ty: Thor's starting Y position
-light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
-
-thorCurrPos = [initial_tx,initial_ty]
-# game loop
+import sys, math
+lx, ly, tx, ty = [int(i) for i in input().split()]
+DX = lx - tx
+DY = ly - ty
 while True:
-    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
-    # A single line providing the move to be made: N NE E SE S SW W or NW
-    
-        if thorPos[0]>light_x:
-        hori = "W"
-        thorPos[0]-=1
-    elif thorPos[0]<light_x:
-        hori = "E"
-        thorPos[0]+=1
-    else:
-        hori = ""
-    if thorPos[1]>light_y:
-        vert = "N"
-        thorPos[1]-=1
-    elif thorPos[1]<light_y:
-        vert = "S"
-        thorPos[1]+=1
-    else:
-        vert = ""
-    print (vert+hori)
+    E = int(input())
+    v, h = "", ""
+    if DX > 0:
+        h = "E"
+        DX += 1 
+    elif DX < 0:
+        h = "W"
+        DX -= 1
+    if DY > 0:
+        v = "S"
+        DY -= 1 
+    elif DY < 0:
+        v = "N"
+        DY += 1
+    print(v+h)
