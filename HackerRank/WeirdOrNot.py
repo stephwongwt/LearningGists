f7f5e86b526943444719bcf6cc04fba1d3ac5af3
Steph Wong <12522345+stephwongwt@users.noreply.github.com>
2018-07-21 14:57:58 +0800

    Create WeirdOrNot.py
    HackerRank 30 day challenge. Day 3.

---
+++ HackerRank/WeirdOrNot.py
@@ -0,0 +1,18 @@
+!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if (N % 2 == 1): #odd
+        print("Weird")
+    else:
+        if (6 <= N <= 20):
+            print("Weird")
+        else:
+            print("Not Weird")
